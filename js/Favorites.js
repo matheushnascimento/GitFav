bdbf0f269ef63eba4d5d861ddef0f70e5479d659
matheushnascimento <matheus.mhddn@gmail.com>
2023-11-22 14:06:08 -0300

    alteração de estilo do botão adicionar

--- js/Favorites.js
+++ js/Favorites.js
@@ -2,6 +2,14 @@ export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
   }
+
+  load() {}
+
+  save() {}
+
+  add() {}
+
+  delete() {}
 }
 
 export class FavoritesView extends Favorites {
@@ -9,21 +17,64 @@ export class FavoritesView extends Favorites {
     super(root);
 
     this.tbody = this.root.querySelector("table tbody");
+    this.star = document.querySelector("svg");
 
     this.update();
+    this.oandd();
+    this.buttonChange();
   }
 
-  oandd() {}
+  oandd() {
+    const favoriteButton = this.root.querySelector("button");
+
+    favoriteButton.onclick = () => {};
+  }
 
   update() {
     this.removeAllTr();
   }
 
-  createRow() {}
+  createRow() {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+    <td class="user">
+    <img
+      src="https://github.com/matheushnascimento.png"
+      alt="Imagem de Matheus Nascimento"
+    />
+    <div class="user-texts">
+      <a
+        href="https://github.com/matheushnascimento"
+        target="_blank"
+        >Matheus Nascimento</a
+      >
+      <p>/matheushnascimento</p>
+    </div>
+  </td>
+  <td>123</td>
+  <td>1234</td>
+  <td class="action">Remover</td>
+  `;
+
+    return tr;
+  }
 
   removeAllTr() {
     this.tbody.querySelectorAll("tr").forEach(tr => {
       tr.remove();
     });
+  }
+
+  buttonChange() {
+    const button = document.querySelector("button");
+    const img = button.querySelector("img");
+
+    button.onmouseover = () => {
+      img.src = "assets/button-blue-star.svg";
+    };
+    button.onmouseout = () => {
+      img.src = "assets/button-star.svg";
+    };
   }
 }
