2e11e071c93417ea75e03c3f9448afc21d70f452
matheushnascimento <matheus.mhddn@gmail.com>
2023-11-22 15:04:57 -0300

    atualização da tabela

--- js/Favorites.js
+++ js/Favorites.js
@@ -9,11 +9,26 @@ export class Favorites {
     this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
   }
 
-  save() {}
+  save() {
+    localStorage.setItem("@github-favorites:", JSON.stringify(this.entries));
+  }
 
   async add(username) {
-    const x = await GithubUser.search(username);
-    console.log(x);
+    try {
+      const userEsists = this.entries.find(entry => entry.login === username);
+
+      if (userEsists) throw new Error("Usuário já cadastrado");
+
+      const user = await GithubUser.search(username);
+
+      if (user.login === undefined) throw new Error("Usuário não encontrado!");
+
+      this.entries = [user, ...this.entries];
+      this.update();
+      this.save();
+    } catch (error) {
+      alert(error.message);
+    }
   }
 
   delete() {}
@@ -45,6 +60,22 @@ export class FavoritesView extends Favorites {
 
   update() {
     this.removeAllTr();
+
+    this.entries.forEach(user => {
+      const row = this.createRow();
+      row.querySelector(
+        ".user img"
+      ).src = `https://github.com/${user.login}.png`;
+
+      row.querySelector(".user img").alt = `Imagem de ${user.name}`;
+      row.querySelector(".user a").href = `https://github.com/${user.login}`;
+      row.querySelector(".user a").textContent = user.name;
+      row.querySelector(".user p").textContent = `/${user.login}`;
+      row.querySelector(".repositories").textContent = user.public_repos;
+      row.querySelector(".followers").textContent = user.followers;
+
+      this.tbody.append(row);
+    });
   }
 
   createRow() {
@@ -65,8 +96,8 @@ export class FavoritesView extends Favorites {
       <p>/matheushnascimento</p>
     </div>
   </td>
-  <td>123</td>
-  <td>1234</td>
+  <td class="repositories">123</td>
+  <td class="followers">1234</td>
   <td class="action">Remover</td>
   `;
 
